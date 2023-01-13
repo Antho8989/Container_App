42fe3d4fbb3391c255997f4e2c93559568a7c582
antho8989 <aogunlana8@gmail.com>
2023-01-13 11:54:13 -0600

    backend update

--- container_app_main/main.tf
+++ container_app_main/main.tf
@@ -32,7 +32,7 @@ provider "azurerm" {
   client_id       = var.SP_CLIENT_ID
   client_secret   = var.SP_CLIENT_SECRET
   tenant_id       = var.SP_TENANT_ID
-  version         = "=2.0.0" #Can be overide as you wish
+  version         = ">= 0.14.11"#Can be overide as you wish
   features {}
 }
 
